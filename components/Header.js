99350e692805535a8ae69bb38f7fa80bb1c1ccf5
Enzo Vergara <enzo.vbracho@gmail.com>
2023-02-07 22:05:08 -0300

    header aun en desarrollo

--- components/Header.js
+++ components/Header.js
@@ -1,45 +1,93 @@
 import Link from "next/link";
-// import styles from "../styles/Header.module.css"
+import styles from "../styles/Header.module.css";
+import Image from "next/image";
+import terrabellum from "../public/terrabellum.png";
 
-const Header = () =>{
-    return(
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"/></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+const Header = () => {
+  return (
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <button type="button" class="btn btn-light">
+          Token
+        </button>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <button type="button" class="btn btn-light">
+                Personajes
+              </button>
+            </li>
+            <li class="nav-item">
+              <button type="button" class="btn btn-light">
+                Ajiooo
+              </button>
+            </li>
+            <div className={styles.head}>
+              <Image
+                width={300}
+                height={120}
+                top={400}
+                left={500}
+                src={terrabellum}
+                alt="lol"
+              />
+            </div>
+            {/* <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <ul class="dropdown-menu">
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Action
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Another action
+                  </a>
+                </li>
+                <li>
+                  <hr class="dropdown-divider" />
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    Something else here
+                  </a>
+                </li>
+              </ul>
+            </li> */}
+            {/* <li class="nav-item">
+              <a class="nav-link disabled">Disabled</a>
+            </li> */}
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-    )
-}
+          <button type="button" class="btn btn-light">
+            MMMMMM
+          </button>
+          <button type="button" class="btn btn-light">
+            Marketplace
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,20 @@
-import Head from 'next/head'
+import Head from "next/head";
 // import { Inter } from '@next/font/google'
 // const inter = Inter({ subsets: ['latin'] })
-import Link from 'next/link'
-import Layout from '../components/layout'
-import Image from 'next/image'
-import espacio from '../public/espacio.png'
-import styles from '../styles/home.module.css'
+import Link from "next/link";
+import Layout from "../components/layout";
+import Image from "next/image";
+import espacio from "../public/espacio.png";
+import styles from "../styles/home.module.css";
 export default function Home() {
   return (
-    
     <>
-    <button className='btn btn-primary'>boton de inicio</button>
-    <Layout titulo="Home">
+      <Layout titulo="Home"></Layout>
+      {/* <Image src="/espacio.png" alt='fondo de la galaxia' Layout='responsive'  objetFit='cover' width={13000 / 10} height={90000 / 10}/> */}
 
-    </Layout>
-    {/* <Image src="/espacio.png" alt='fondo de la galaxia' Layout='responsive'  objetFit='cover' width={13000 / 10} height={90000 / 10}/> */}
-
-    <div className={styles.fondo}  >
-      <Image src={espacio} alt='lol' Layout='relative' quality={20}/>
-    </div>
-    
+      <div className={styles.fondo}>
+        <Image src={espacio} alt="lol" Layout="relative" quality={20} />
+      </div>
     </>
-   
-  )
+  );
 }
