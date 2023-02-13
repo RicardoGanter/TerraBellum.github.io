2431aa98d62e54130120b64071a76c31a244b7d6
Enzo Vergara <enzo.vbracho@gmail.com>
2023-02-13 17:22:02 -0300

    iconos a color

--- components/footer.js
+++ components/footer.js
@@ -1,42 +1,94 @@
-import styles from '../styles/footer.module.css'
-import Image from 'next/image';
-import discord from '../public/icon/discordgris.svg'
-import instagram from '../public/icon/instagram.svg'
-import tiktok from '../public/icon/tiktok.svg'
-import facebook from '../public/icon/facebookgris.svg'
-import twiter from '../public/icon/twiter.svg'
-import TB from '../public/img/tb.png'
-import Link from 'next/link';
-import ImageHover from './imagenRsociales';
-import { useState } from 'react';
+import styles from "../styles/footer.module.css";
+import Image from "next/image";
+import discord from "../public/icon/discord.svg";
+import discordcolor from "../public/icon/discordcolor.svg";
+import instagram from "../public/icon/instagram.svg";
+import instgramcolor from "../public/icon/instagramcolor.svg";
+import tiktok from "../public/icon/tiktok.svg";
+import tiktokcolor from "../public/icon/tiktokcolor.svg";
+import facebook from "../public/icon/facebook.svg";
+import facebookblue from "../public/icon/facebookblue.svg";
+import twitter from "../public/icon/twitter.svg";
+import twitterblue from "../public/icon/twitterblue.svg";
+import TB from "../public/img/tb.png";
+import Link from "next/link";
+import ImageHover from "./imagenRsociales";
+import { useState } from "react";
 
-const Footer = ()=>{
-    
-    return(
-        <>
-            <div className={styles.redesSociales}>
-                <Link href={'/'}><Image src={TB} width={350} alt='Terrabellum'/></Link>
-                <div>
-                <Link href={'https://www.instagram.com/Terra_Bellum/'} ><ImageHover className={styles.svgRedesSociales}
-                initialImage={instagram} alternativeImage={discord} /></Link>
-                <Link href={'https://www.instagram.com/Terra_Bellum/'}><ImageHover className={styles.svgRedesSociales}
-                initialImage={instagram} alternativeImage={discord}/></Link>
-                <Link href={'https://twitter.com/TerraBellum_NFT'}><ImageHover className={styles.svgRedesSociales}
-                initialImage={instagram} alternativeImage={discord}/></Link>
-                <Link href={'google.com'}><ImageHover className={styles.svgRedesSociales}
-                initialImage={instagram} alternativeImage={discord}/></Link>
-                <Link href={'https://www.facebook.com/profile.php?id=100082027137863'}><ImageHover className={styles.svgRedesSociales}
-                initialImage={instagram} alternativeImage={discord}/></Link>
-                </div>
-            
-            <div className={styles.footertext}>
-                <Link className={styles.enlacesfooter} href={"https://terrabellum.gitbook.io/whitepaper-terrabellum/"}>Whitepaper</Link>
-                <Link className={styles.enlacesfooter} href={"youtube.com"}>FAQ</Link>
-                <Link className={styles.enlacesfooter} href={"youtube.com"}>Privacy Policy</Link>
-                <Link className={styles.enlacesfooter} href={"youtube.com"}>Terms of Use</Link>
-            </div>
-            </div>
-        </>
-)}
+const Footer = () => {
+  return (
+    <>
+      <div className={styles.redesSociales}>
+        <Link href={"/"}>
+          <Image src={TB} width={350} alt="Terrabellum" />
+        </Link>
+        <div>
+          <Link
+            className={styles.icon}
+            href={"https://twitter.com/TerraBellum_NFT"}
+          >
+            <ImageHover
+              className={styles.svgRedesSociales}
+              initialImage={twitter}
+              alternativeImage={twitterblue}
+            />
+          </Link>
+          <Link
+            className={styles.icon}
+            href={"https://www.instagram.com/Terra_Bellum/"}
+          >
+            <ImageHover
+              className={styles.svgRedesSociales}
+              initialImage={instagram}
+              alternativeImage={instgramcolor}
+            />
+          </Link>
+          <Link
+            className={styles.icon}
+            href={"https://facebook.com/TerraBellum_NFT"}
+          >
+            <ImageHover
+              className={styles.svgRedesSociales}
+              initialImage={facebook}
+              alternativeImage={facebookblue}
+            />
+          </Link>
+          <Link className={styles.icon} href={"tiktok.com"}>
+            <ImageHover
+              className={styles.svgRedesSociales}
+              initialImage={tiktok}
+              alternativeImage={tiktokcolor}
+            />
+          </Link>
+          <Link className={styles.icon} href={"https://www.discord.com"}>
+            <ImageHover
+              className={styles.svgRedesSociales}
+              initialImage={discord}
+              alternativeImage={discordcolor}
+            />
+          </Link>
+        </div>
+
+        <div className={styles.footertext}>
+          <Link
+            className={styles.enlacesfooter}
+            href={"https://terrabellum.gitbook.io/whitepaper-terrabellum/"}
+          >
+            Whitepaper
+          </Link>
+          <Link className={styles.enlacesfooter} href={"youtube.com"}>
+            FAQ
+          </Link>
+          <Link className={styles.enlacesfooter} href={"youtube.com"}>
+            Privacy Policy
+          </Link>
+          <Link className={styles.enlacesfooter} href={"youtube.com"}>
+            Terms of Use
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Footer;
