43578ade1db6d64b85bcf0ba6448d12c01fec395
RicardoRosales <ricardorosalesganter@gmail.com>
2023-02-15 18:29:00 -0300

    .

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,12 @@ import Texto from '@/components/texto.jsx'
 import Footer from '@/components/footer'
 import TextoCloacas from "@/components/miembros";
 import Nftcartas from "@/components/nftcartas.jsx";
+import InicialText from "@/components/inicialTexto";
 export default function Home() {
   return (
     <>
     <Layout titulo="Home"/>
+    <InicialText/>
     <Texto/>
     <Nftcartas/>
     <TextoCloacas/>
