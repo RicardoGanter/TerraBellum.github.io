c9bbc2519ac3af4f03e532b9f3a4c792806472f5
RicardoRosales <ricardorosalesganter@gmail.com>
2023-02-17 13:35:40 -0300

    corrigiendo footer

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,20 @@
 import Layout from '../components/layout'
-// import Texto from '@/components/texto.js'
+import Texto from '@/components/texto.js'
 import Footer from '@/components/footer'
 import TextoCloacas from "@/components/miembros";
 import Nftcartas from "@/components/nftcartas.jsx";
 import InicialText from "@/components/inicialTexto";
 export default function Home() {
   return (
-    <>
+    <div style={{display:"flex", flexDirection:"column"}}>
+    <main>
     <Layout titulo="Home"/>
     <InicialText/>
-    {/* <Texto/> */}
+    <Texto/>
     <Nftcartas/>
     <TextoCloacas/>
+    </main>
     <Footer/>
-    </>
+    </div>
   )
 }
