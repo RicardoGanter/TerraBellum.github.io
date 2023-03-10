5620d6f56d0ab13de7e9c70dba3bdda949e68493
Enzo Vergara <enzo.vbracho@gmail.com>
2023-03-10 14:46:59 -0300

    esta responsivo dale un vistazo si quieres y bueno el grafico f:(

--- components/Chart.js
+++ components/Chart.js
@@ -67,7 +67,6 @@ const myChart = new Chart(ctx, {
           "rgba(255, 206, 86, 0.2)",
           "rgba(75, 192, 192, 0.2)",
           "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)",
         ],
         borderColor: [
           "rgba(255, 99, 132, 0.2)",
