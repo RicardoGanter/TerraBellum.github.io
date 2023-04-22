7c0fca23d4fdff1e6f9bda8f895dd60a9f5467c1
Ricardo Ganter <ricardorosalesganter@gmail.com>
2023-04-22 16:54:30 -0400

    cambio temporal de config-next

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,6 +1,8 @@
-const Loading = ()=>{
-    <h1>
-        Loading...
-    </h1>
+export default function Loading(){
+   
+    return (
+        <h1 style={{backgroundColor:"blue", padding:"50rem", zIndex:4, fontSize:400}}>
+            Loading...
+        </h1>
+    )
 }
-export default Loading
--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
     appDir: true
   },
   output: 'export',
-  images: { unoptimized: true } 
+  images: { unoptimized: true } ,
+  outputTracing: 'all'
 }
 module.exports = nextConfig
